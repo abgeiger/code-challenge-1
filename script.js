7afe0a5d055d0242e3a4592b46863cdac3de9700
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-13 13:37:02 -0600

    completed challenge

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('js');
+var clickCount = 0;
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('JQ');
+    $(document).on('click', '#generateButton', generateButtonClick);
+    $(document).on('click', '.swapButton', swapButtonClick);
+    $(document).on('click', '.swapButton', swapButtonClick);
+    $(document).on('click', '.swapButton2', swapButtonClick2);
+    $(document).on('click', '.deleteButton', deleteButtonClick);
+}
+
+function deleteButtonClick() {
+    $(this).parent().remove();
+}
+
+function generateButtonClick() {
+    clickCount++;
+    $('body').append('<div class="redDiv"><p>' + clickCount + 
+    '</p><button class="swapButton">Swap</button><button class="deleteButton">Delete</button></div');
+}
+
+function swapButtonClick() {
+    $(this).parent().css('background-color', 'yellow');
+    $(this).replaceWith('<button class="swapButton2">Swap</button>');
+}
+
+function swapButtonClick2() {
+    $(this).parent().css('background-color', 'red');
+    $(this).replaceWith('<button class="swapButton">Swap</button>');
+}
